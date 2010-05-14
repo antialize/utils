4f3ad26111fd0719d8ad98897ada40f21b80ff3e
Antialize <antialize@gmail.com>
2010-05-14 16:04:21 +0200

    Added podder

---
+++ podder.py
@@ -0,0 +1,129 @@
+#!/usr/bin/python
+import smtplib
+import mimetypes
+from email.mime.audio import MIMEAudio
+from email.mime.application import MIMEApplication
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email import encoders
+
+import libxml2
+from urllib import urlopen
+import os
+from subprocess import call,Popen, PIPE
+from os import listdir,mkdir,remove
+from multiprocessing import Pool
+from tempfile import NamedTemporaryFile
+from hashlib import sha1
+from conf import *
+
+def getext(file):
+    mimemap = {"video/mp4": "mp4", "video/x-flv": "flv", "video/quicktime" : "mov",
+               "application/ogg": "ogg", "video/x-ms-asf": "asf", 
+               "video/x-msvideo": "avi", "application/vnd.rn-realmedia": "rm"}
+    mimetype = Popen(["file",'-b','--mime-type',file], stdout=PIPE).communicate()[0].strip()
+    if mimetype in mimemap: return mimemap[mimetype]
+    magic = Popen(["file",'-b',file], stdout=PIPE).communicate()[0].strip()
+    if magic == "Matroska data": return "mkv"
+    return "mpg"
+
+def fixname(name):
+    return name.replace("\\","_").replace("#","_").replace("!","_").replace("/","-")
+
+#def tomkv(src, dst):
+    #If mkvmerge works just use that
+#    if call(["mkvmerge", src, '-q', '-o', dst]) == 0: return True
+    #If not we will use mencoder
+#    tmp = sha1(dst).hexdigest()
+#    if call(["mencoder",src,"-really-quiet","-oac","copy","-ovc","copy","-o",tmp]) != 0: return False
+    #The output of mencoder does not seem to be propperly muxed
+#    if call(["mkvmerge",tmp, '-q', '-o', dst]) != 0: return False
+#    remove(tmp)
+#    return True
+
+def youtubedownload(src, dst):
+    tmp = sha1(src).hexdigest()
+    print "Fetching %s"%dst
+    if call(["youtube-dl",src,'-c', '-b','-q', '-o', tmp]) != 0: return False
+    os.rename(tmp,dst+"."+getext(tmp))
+    #print "mkv'ing %s"%dst
+    #if not tomkv(tmp, dst): return False
+    #remove(tmp)
+    return True
+
+def downloadwget(src, dst):
+    tmp = sha1(src).hexdigest()
+    print "Fetching %s"%dst
+    if call(["wget", src, '-c', "-q", "-O", tmp]) != 0: return False
+    os.rename(tmp,dst+"."+getext(tmp))
+    #print "mkv'ing %s"%dst
+    #if not tomkv(tmp, dst): return False
+    #remove(tmp)
+    return True
+
+def downloadday9(pool):
+    global vf
+    doc = libxml2.parseDoc(urlopen("http://day9tv.blip.tv/rss").read())
+    ctxt = doc.xpathNewContext()
+    
+    files = set([ x.rsplit(".",1)[0] for x in listdir("day9")])
+    
+    for x in ctxt.xpathEval("//item"):
+        url = x.xpathEval("enclosure/@url")
+        title = x.xpathEval("title")
+        if url and title:
+            t = fixname(title[0].getContent().replace("D9D ","").replace("Day[9] Daily #","").replace("-"," ",1))
+            t = t.split(" ",1)
+        t = "%03d - %s"%(int(t[0]),t[1].strip())
+        if not t in files:
+            src = url[0].getContent()
+            vf.append( (user, t) )
+            pool.apply_async(downloadwget, (src,"day9/"+t))
+            #downloadwget(src,t)
+    
+def downloadyoutubeuser(pool, user):
+    global vf
+    print "========================> %s <==========================="%user
+    doc = libxml2.parseDoc(urlopen("http://gdata.youtube.com/feeds/base/users/%s/uploads?alt=rss&v=1&orderby=published"%user).read())
+    ctxt = doc.xpathNewContext()
+        
+    videos = []
+    for x in ctxt.xpathEval("//item"):
+        url = x.xpathEval("link")
+        title = x.xpathEval("title")
+        if url and title:
+            videos.append( (url[0].getContent(), fixname(title[0].getContent())) )
+
+            try: mkdir(user) 
+            except Exception: pass
+        
+    videos.reverse()    
+    dir = listdir(user)
+    numtoname = {}
+    nametonum = {'dummy':0}
+    numtofullname = {}
+    for x in dir:
+        n, name = x.split(' - ',1)
+        numtofullname[int(n)] = name
+        name = name.rsplit(".",1)[0]
+        numtoname[int(n)] = name
+        nametonum[name] = int(n)
+
+    if videos[0][1] in nametonum: video=nametonum[videos[0][1]]
+    else: video=max(nametonum.values())+1
+
+    i=0
+    while i < len(videos) or (i+video) in numtoname:
+        v = (i+video)
+        if v in numtoname and numtoname[v] == videos[i][1]: 
+            i += 1
+            continue
+
+        if v in numtoname: 
+            remove("%s/%04d - %s"%(user,v,numtofullname[v]))
+
+        if i < len(videos): 
+            vf.append( (user, "%04d - %s"%(v,videos[i][1])) )
+            pool.apply_async(youtubedownload, (videos[i][0],"%s/%04d - %s"%(user,v,videos[i][1])))
+            #youtubedownload(videos[i][0],"%s/%04d - %s"%(use
